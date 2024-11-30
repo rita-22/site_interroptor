c846fee0edacaf28f93fd0df521c122c13798496
rita-22 <ana2rita.m@gmail.com>
2024-11-30 23:03:56 +0000

    Corrigido
    site totalmente pronto

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ const body = document.body;
 const interruptor = document.getElementById("interruptor");
 
 // Determinar o estado inicial com base na classe do body
-let luzAcesa = body.classList.contains("acesa");
+let luzAcesa = false;
 
 // Configurar o texto inicial do botão
-interruptor.textContent = luzAcesa ? "Apagar" : "Acender";
 
 // Adicionar o evento de clique para alternar o estado da luz
 interruptor.addEventListener("click", () => {
-    luzAcesa = !luzAcesa; // Alterna o estado da luz
 
     if (luzAcesa) {
         body.classList.remove("apagada");
         body.classList.add("acesa");
         interruptor.textContent = "Apagar"; // Atualiza o texto do botão
+        luzAcesa = !luzAcesa; // Alterna o estado da luz
+
     } else {
         body.classList.remove("acesa");
         body.classList.add("apagada");
         interruptor.textContent = "Acender"; // Atualiza o texto do botão
+        luzAcesa = !luzAcesa; // Alterna o estado da luz
+
     }
 });
 
